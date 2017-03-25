85d9dac827ed6156f69d9c337d872c549b16a12c
bwuzahng <bwuzhang@gmail.com>
2017-03-25 18:38:21 -0400

    Added a mog to run on ug machines

--- A3/code/MoG.py
+++ A3/code/MoG.py
@@ -3,97 +3,103 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils import *
 
+points_100d = np.load("../data/data100D.npy")
+Number_of_data = points_100d.shape[0]
+validSize = int(Number_of_data * 1.0 / 3.0)
+validData = points_100d[:validSize,:]
+trainData = points_100d[validSize:,:]
 
-data = np.load("../data/data2D.npy")
-DATA_DIMENSION = 2
-points_2d = data
-x = points_2d[:,0]
-y = points_2d[:,1]
-color_list = ["g", "b", "r", "y", "k"]
-
-def get_covariance_mat(sigma):
-
-    return tf.matrix_diag(tf.square(sigma))
-
-# oneDivSqrtTwoPI = 1 / math.sqrt(2*tf.math.pi)
-def get_mul_normal(x, mu, sigma):
+def get_log_gaussian(X, mu, diag_stdev):
     '''
-    x should be in [N, DATA_DIMENSION]. mu should be in [K, DATA_DIMENSION].
-    sigma should be in [K, DATA_DIMENSION]
-
-    It returns a [N, K] matrix representing the pdf value
+    X: BxD
+    mu: KxD
+    diag_stdev: KxD
+    B: number of data points
+    D: dimension of a data point
+    K: number of clusters
+    return BxK matrix
     '''
+    B = tf.shape(X)[0]
+    D = tf.shape(X)[1]
+    K = tf.shape(mu)[0]
+    rep_X = tf.reshape(tf.tile(X, [1, K]), [B, K, D])
+    dist = tf.contrib.distributions.MultivariateNormalDiag(mu, diag_stdev)
+    return dist.log_pdf(rep_X)
+    
+def get_log_P(X, pi, mu, diag_stdev):
+    '''
+    X: BxD
+    pi: 1xK
+    mu: KxD
+    diag_stdev: Kx1
+    B: number of data points
+    D: dimension of a data point
+    K: number of clusters
+    return float64
+    '''
+    B = tf.shape(X)[0]
+    D = tf.shape(X)[1]
+    K = tf.shape(mu)[0]
+    rep_pi = tf.reshape(tf.tile(pi, [1,B]), [B, K])
+    log_gaussian = get_log_gaussian(X, mu, diag_stdev)
+    res = tf.reduce_sum(reduce_logsumexp(rep_pi + log_gaussian))
+    return res
 
-    print x.get_shape().as_list()
-    print mu.get_shape().as_list()
-    print sigma.get_shape().as_list()
+def get_MoG_assign(X, pi, mi, diag_stdev):
+    assign = tf.argmax(get_log_P_Z_given_X(X, pi, mu, diag_stdev), 1)
+    return assign
 
-    assert x.get_shape().as_list()[1] == mu.get_shape().as_list()[1]
-    assert mu.get_shape().as_list()[1] == DATA_DIMENSION
-
-    N = x.get_shape().as_list()[0]
-    K = mu.get_shape().as_list()[0]
-    D = x.get_shape().as_list()[1]
-
-    # print type([N, K, D][0])
-    # print type([1,2,3][0])
-    # Covariance shouls be in [K, DATA_DIMENSION, DATA_DIMENSION]
-    covariance = get_covariance_mat(sigma)
-
-    tmp = tf.tile(x, [1, K])
-    x_sub_mu = tf.reshape(tmp, [-1, K, D]) - mu
-
-    # print x_sub_mu.get_shape().as_list()
-
-    expo = tf.exp(-0.5 * tf.reduce_sum(tf.multiply(tf.transpose(tf.matmul(tf.transpose(x_sub_mu,[1,0,2]),tf.matrix_inverse(covariance)),[1,0,2]),x_sub_mu),2))
-    ret = tf.pow(2 * np.pi, tf.cast(-tf.shape(x)[1], tf.float32) / 2) * tf.pow(tf.sqrt(tf.reduce_sum(tf.square(covariance))), - 0.5) * expo
-
-    return ret
-
-def buildGraph_mog_Adam(K, learning_rate):
+def buildGraph_MoG_Adam_100d(K, learning_rate):
     # Variable creation
-    points = tf.placeholder(tf.float32, [None, 2], name='input_points')
-    mu = tf.Variable(tf.truncated_normal(shape=[K,2], stddev=1), name='mu')
-    # sigma = tf.Variable(tf.truncated_normal(shape=[K,2], stddev=0.5), name='sigma')
-    sigma = tf.Variable(tf.ones([K,2]), name='sigma')
-    pi =  tf.Variable(tf.div(tf.ones([K]),K), name="pi")
+    points = tf.placeholder(tf.float32, [None, 100], name='input_points')
+    mu = tf.Variable(tf.truncated_normal(shape=[K,100], stddev=0.5), name='mu')
 
-    N = tf.shape(points)[0]
+#     diag_stdev = tf.Variable(tf.exp(tf.truncated_normal(shape=[K,100], stddev=0.5)), name='diag_stdev')
 
-    pdf = get_mul_normal(points, mu, sigma)
-    print tf.shape(pdf)
-    loss = -tf.reduce_sum(reduce_logsumexp(pi * pdf))
+#     Covariance matrix
+#     diag_stdev = tf.Variable(tf.exp(tf.truncated_normal(shape=[K,100], stddev=0.5)), name='diag_stdev')
+#     print "Using covariance matrix"
 
+#     Single sigma value
+    sigma = tf.Variable(tf.exp(tf.truncated_normal(shape=[K], stddev=0.5)), name='sigma')
+    diag_stdev = tf.transpose(tf.reshape(tf.tile(sigma, [100], name='diag_stdev'),[100,K]))
+    print "Using single sigma"
+
+    phi = tf.Variable(tf.truncated_normal(shape=[1,K], stddev=0.5), name='phi')
+    pi = logsoftmax(phi)
+
+    # Loss definition
+    loss = -get_log_P(points, pi, mu, diag_stdev)
     # Training mechanism
     optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate, beta1=0.9, beta2=0.99, epsilon=1e-5)
     train = optimizer.minimize(loss=loss)
-    return points, mu, loss, train, pdf
+    return points, pi, mu, diag_stdev, loss, train
 
-learning_rate = 0.1
-K = 3
-points, mu, loss, train, pdf = buildGraph_mog_Adam(K, learning_rate)
-init = tf.global_variables_initializer()
-sess = tf.InteractiveSession()
-sess.run(init)
+K_list = [5,10,15,20,25]
+learning_rate = 0.01
+color_list = ["g", "b", "r", "y", "k"]
+number_of_valid_data = validData.shape[0]
 loss_recorder = np.array([])
 
-numIteration = 500
-for itr in range(numIteration):
-    loss_, _, prob, centroid = sess.run([loss, train, pdf, mu], feed_dict={points: points_2d})
+for K in K_list:
+    tf.reset_default_graph()
+    points, pi, mu, diag_stdev, loss, train = buildGraph_MoG_Adam_100d(K, learning_rate)
+    init = tf.global_variables_initializer()
+    sess = tf.InteractiveSession()
+    sess.run(init)
+
+    numIteration = 600
+    pi_, mu_, diag_stdev_ = [], [], []
+    for itr in range(numIteration):
+        pi_,mu_,diag_stdev_,_ = sess.run([pi, mu, diag_stdev, train], feed_dict={points: trainData})
+        loss_ = sess.run(loss, feed_dict={points: validData})
+        if itr % 100 == 0:
+            print("Number of cluster: %d, Iteration#: %d, Validation loss: %0.2f"%(K, itr, loss_))
+    loss_ = sess.run(loss, feed_dict={points: validData})
     loss_recorder = np.append(loss_recorder, loss_)
-    if itr % 100 == 0:
-        print("Iteration#: %d, loss: %0.2f"%(itr, loss_))
-# plt.plot(np.arange(numIteration), loss_recorder, 'g')
-# #plt.axis([0,500, 0, 2])
-# plt.title("Total loss VS number of updates for learning_rate = %0.3f"%(learning_rate))
-# plt.show()
-# print prob[0:10,:]
-        assign = np.argmax(prob, axis = 1)
-        colors = [color_list[assign[i]] for i in range(len(x))]
-        plt.scatter(x,y,c=colors)
-# print centroid.shape
-        colors = ['k' for i in range(centroid.shape[0])]
-        plt.scatter(centroid[:,0],centroid[:,1],c=colors)
-        #plt.axis([0,500, 0, 2])
-        plt.title("K mean plots for k = %d"%(K))
-        plt.show()
+
+
+plt.plot(K_list, loss_recorder, 'g')
+    #plt.axis([0,500, 0, 2])
+plt.title("Total loss of validation set VS number of clusters K")
+plt.show()
